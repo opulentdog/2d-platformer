dbb5016bab6810d4ede225688ba62c703d10b0bb
Nathan Lelégard <nathan.lelegard@telecom-sudparis.eu>
2026-02-19 00:00:22 +0100

    Attributs de la classe Sound en private

--- src/main/java/tsp/graphics/Sound.java
+++ src/main/java/tsp/graphics/Sound.java
@@ -7,8 +7,8 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Sound {
-	Clip clip;
-	URL soundURL[] = new URL[30];
+	private Clip clip;
+	private URL soundURL[] = new URL[30];
 	
 	public Sound() {
 		soundURL[0] = getClass().getResource("/sounds/track1.wav");
