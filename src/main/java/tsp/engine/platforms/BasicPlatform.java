729ba02c659004f79a4800077328de45938b015b
Nathan Lelégard <nathan.lelegard@telecom-sudparis.eu>
2026-02-26 13:31:56 +0100

    BasicPlatform se construit en chargeant le son Jump sur l'attribut Sound soundeff

--- src/main/java/tsp/engine/platforms/BasicPlatform.java
+++ src/main/java/tsp/engine/platforms/BasicPlatform.java
@@ -1,9 +1,13 @@
 package tsp.engine.platforms;
 
+import tsp.graphics.Sound;
+
 public class BasicPlatform extends Platform{
 	
 	public BasicPlatform() {
 		super("/images/platform.png", 100, 30);
+		this.soundeff = new Sound("/sounds/sound_effect/SFX_Jump_42.wav.wav");
+
 	}
 
 }
