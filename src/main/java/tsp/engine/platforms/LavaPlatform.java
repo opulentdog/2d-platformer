2e07b529978a5ef2dd612c90e2262c7527cad017
Nathan Lelégard <nathan.lelegard@telecom-sudparis.eu>
2026-03-10 17:09:11 +0100

    Ajout du sound effect de la lava plateform et initialisation de son attribut souneff

--- src/main/java/tsp/engine/platforms/LavaPlatform.java
+++ src/main/java/tsp/engine/platforms/LavaPlatform.java
@@ -2,11 +2,13 @@ package tsp.engine.platforms;
 
 import tsp.engine.Player;
 import tsp.engine.platforms.Platform.PlatformType;
+import tsp.graphics.Sound;
 
 public class LavaPlatform extends Platform {
 	
 	public LavaPlatform() {
 		super("/images/platform-lava.png", 100, 30);
+		this.soundeff = new Sound("sounds/sound_effect/8-bit-bomb-explosion-2811.wav");
 		this.type = PlatformType.LAVA;
 	}
 	
