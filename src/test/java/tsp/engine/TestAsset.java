982b91fb227f2843dedfaf35b2933411614bc5ad
Vince51350 <vincent.ducray@telecom-sudparis.eu>
2026-03-27 19:03:13 +0100

    Ajout fichier test Nathan

--- src/test/java/tsp/engine/TestAsset.java
+++ src/test/java/tsp/engine/TestAsset.java
@@ -3,7 +3,11 @@ package tsp.engine;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import tsp.engine.platforms.Platform;
+import tsp.engine.platforms.BasicPlatform;
 
 /**
  * Unit Test Class for {@link tsp.engine.Asset}
@@ -13,18 +17,46 @@ public class TestAsset {
 	* The {@link Asset} object to be tested. It must be initialised to a concrete
 	* instance in the {@link #setUp()} method.
 	*/
-	protected Asset assetUnderTest;
+	protected Asset playerUnderTest1;
 
-	  
 	 /**
 	 * The {@link Asset} object to be tested. It must be initialised to a concrete
 	 * instance in the {@link #setUp()} method.
 	 */
-	 protected Asset assetUnderTest;
+	 protected Asset playerUnderTest2;
+	 
+	 protected Platform platformUnderTest1;
 	
 	  @BeforeEach
 	  public void setUp() {
-	    assetUnderTest= new Asset("/images/player.png", 70, 70);
+		  playerUnderTest1= new Player("/images/player.png", 70, 70);
+		  platformUnderTest1 = new BasicPlatform();
+	  }
+	  
+	  /**
+	   * Test1: Contact entre un Player et une plateforme
+	   */
+	  @Test
+	  @DisplayName("Test 1: Contact entre un Player et une plateforme")
+	  void platformOverPlayer() {
+		playerUnderTest1.x=50;
+		playerUnderTest1.y=50;
+		platformUnderTest1.x=50;
+		platformUnderTest1.y=50;
+	    Assertions.assertTrue(platformUnderTest1.intersects(playerUnderTest1));
+	  }
+	  
+	  /**
+	   * Test2: Player et plateforme éloignés
+	   */
+	  @Test
+	  @DisplayName("Test 2: Player et plateforme éloignés")
+	  void platformFarFromPlayer() {
+		playerUnderTest1.x=50;
+		playerUnderTest1.y=400;
+		platformUnderTest1.x=500;
+		platformUnderTest1.y=50;
+	    Assertions.assertFalse(platformUnderTest1.intersects(playerUnderTest1));
 	  }
 	  
 	  /**
@@ -32,7 +64,7 @@ public class TestAsset {
 	   */
 	  @AfterEach
 	  public void tearDown() {
-		  assetUnderTest = null;
+		  playerUnderTest2 = null;
 	  }
 
 
