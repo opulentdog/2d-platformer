c9a526b8d54da62d73a90c91b17e2b20961abd98
Nathan Lelégard <nathan.lelegard@telecom-sudparis.eu>
2026-04-01 12:54:45 +0200

    Nom des variables PATH changées

--- src/main/java/tsp/graphics/Constants.java
+++ src/main/java/tsp/graphics/Constants.java
@@ -3,11 +3,13 @@ package tsp.graphics;
 public final class Constants {
 	private Constants() {}
 	
-    public static final String CLOUDS = "/images/fond_menu.jpg";
-    public static final String SPACE = "/images/space.jpg";
+    public static final String CLOUDSPATH = "/images/fond_menu.jpg";
+    public static final String SPACEPATH = "/images/space.jpg";
+    public static final String PLAYERPATH = "/images/player.jpg";
+
     
-    public static final String TRACK_1 = "/sounds/music/track1.wav";
-    public static final String TRACK_2 = "/sounds/music/track2.wav";
+    public static final String TRACK1_PATH = "/sounds/music/track1.wav";
+    public static final String TRACK2_PATH = "/sounds/music/track2.wav";
     public static final String GAMEOVER = "/sounds/music/GameOver.wav";
 
 
