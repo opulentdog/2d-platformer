8fb78f534e35f93f6a685242a4572fe2b901a14c
Nathan Lelégard <nathan.lelegard@telecom-sudparis.eu>
2026-06-09 19:19:22 +0200

    Ajout des javadoc des package et de Player

---
+++ src/main/java/tsp/engine/platforms/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Ce package contient les classes du moteur du jeu 2D platformer.
+ * Il gère la logique du jeu, la physique et le comportement des entités.
+ */
+package tsp.engine.platforms;
---
+++ src/main/java/tsp/graphics/render/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Ce package contient les différents renders
+ * Il y a un render par entité de Game, et ils s'occupent de les afficher
+ * Ce sont eux qui sont appelés dans Window et PAS les entités du moteur
+ */
+package tsp.graphics.render;
